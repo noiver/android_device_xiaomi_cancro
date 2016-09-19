1dd2f5e3960307562faa8ce668d3e36e0f304624
noiver <1017227274@qq.com>
2016-09-19 17:14:36 +0800

    initialized repository

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#!env bash
+# -*- coding: utf-8 -*-
+#Filename:    vendorsetup.sh
+#Porject :    
+#Author  :    zerozakiGeek
+#Email   :    zerozakiGeek@gmail.com
+#Date    :    2016-09-01
+add_lunch_combo cm_cancro-userdebug
